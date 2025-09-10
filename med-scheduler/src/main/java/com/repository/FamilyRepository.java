56cb52d40e504af8979ad5693785a39a5ee3b212
Vamsi-PrimeSoft <133194079+Vamsi-PrimeSoft@users.noreply.github.com>
2025-09-10 14:24:29 +0530

    Delete med-scheduler directory

--- med-scheduler/src/main/java/com/repository/FamilyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.medscheduler.repository;
-
-import com.medscheduler.entity.Family;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FamilyRepository extends JpaRepository<Family, Long> {
-}
